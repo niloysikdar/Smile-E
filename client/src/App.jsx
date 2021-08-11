42f749c5249b8766ae675dcf8b9f860ca8f3706b
niloysikdar <niloysikdar30@gmail.com>
2021-08-11 23:43:27 +0530

    Dashboard changes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Switch, Route } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import { Dashboard } from "./pages/Dashboard";
 import JoinUs from "./pages/JoinUs/JoinUs";
 import Partnership from "./pages/Partnership/Partnership";
 import Agreement from "./pages/Agreement";
